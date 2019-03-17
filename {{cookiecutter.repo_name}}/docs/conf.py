3462d804cb52a08b4d95523123e76e6dc37488ff
Brian Grant <brian.grant@urbanairship.com>
2019-03-17 13:27:18 -0700

    update project name var

--- {{cookiecutter.repo_name}}/docs/conf.py
+++ {{cookiecutter.repo_name}}/docs/conf.py
@@ -46,7 +46,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'{{ cookiecutter.project_name }}'
+project = u'{{ cookiecutter.pypi_project_name }}'
 copyright = u'{% now 'local', '%Y' %}, {{ cookiecutter.full_name }}'
 
 # The version info for the project you're documenting, acts as replacement for
